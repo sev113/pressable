92b1884c9188e46919e9f33886d2b507fb3e4c6e
sev113 <nakedsev113@gmail.com>
2023-07-04 13:18:02 +0630

    Started Project

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, StatusBar, Pressable } from "react-native";
 
 export default function App() {
+  const [timePressed, setTimePressed] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable
+        onPress={() => {
+          setTimePressed((current) => current + 1);
+        }}
+        style={({ pressed }) => [
+          { backgroundColor: pressed ? "#0facd4" : "transparent" },
+          { padding: 5, paddingStart: 15, paddingEnd: 15, borderRadius: 5 },
+        ]}
+      >
+        {({ pressed }) => <Text>{pressed ? "Saving..." : "Save"}</Text>}
+      </Pressable>
+      <Text>You have saved this {timePressed} times</Text>
     </View>
   );
 }
@@ -13,8 +25,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: StatusBar.currentHeight,
+  },
+  save: {
+    // backgroundColor: "#d40f75",
+    color: "#d40f75",
+    padding: 10,
+    paddingStart: 15,
+    paddingEnd: 15,
+    borderRadius: 5,
   },
 });
